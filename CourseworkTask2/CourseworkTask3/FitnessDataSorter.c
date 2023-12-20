3ba3e860af200bfd5a2fced32e89006b984219ee
mayamariagupta <146712235+mayamariagupta@users.noreply.github.com>
2023-12-20 10:25:41 +0000

    coursework3

---
+++ CourseworkTask2/CourseworkTask3/FitnessDataSorter.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define the struct for the fitness data
+typedef struct {
+    char date[11];
+    char time[6];
+    int steps;
+} FitnessData;
+
+// Function to tokenize a record
+void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
+    char *ptr = strtok(record, &delimiter);
+    if (ptr != NULL) {
+        strcpy(date, ptr);
+        ptr = strtok(NULL, &delimiter);
+        if (ptr != NULL) {
+            strcpy(time, ptr);
+            ptr = strtok(NULL, &delimiter);
+            if (ptr != NULL) {
+                *steps = atoi(ptr);
+            }
+        }
+    }
+}
+
+
+int main() {
+
+    char filename[100];
+    printf("Enter Filename: ");
+    scanf("%s", filename);
+    FILE *inputfile = fopen(filename, "r");
+    if (inputfile == NULL)
+    {
+        perror("");
+        return 1;
+}
+}
